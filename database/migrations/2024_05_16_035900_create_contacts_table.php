303af677b130af3caa2d1d93018f3f82893229e5
Carl Joseph T. Salac <salaccarljoseph@gmail.com>
2024-05-18 19:52:23 +0800

    feat(migration): add and migrate contacts table then add corresponding model

---
+++ database/migrations/2024_05_16_035900_create_contacts_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('contacts', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('info_id');
+            $table->string('phone_number', 20);
+            $table->string('email', 100);
+            $table->timestamps();
+
+            // Foreign key constraint
+            $table->foreign('info_id')->references('id')->on('infos')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('contacts');
+    }
+};
